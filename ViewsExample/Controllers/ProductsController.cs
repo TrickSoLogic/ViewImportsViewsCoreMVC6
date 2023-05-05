e7aca3a4b8d0d28f981333b03033d900181e5c43
Vishal <vishalbhat.com@gmail.com>
2023-05-05 17:05:30 +0530

    Add project files.

---
+++ ViewsExample/Controllers/ProductsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ViewImportsViewsCoreMVC6.Controllers
+{
+  public class ProductsController : Controller
+  {
+    [Route("products/all")]
+    public IActionResult All()
+    {
+      return View();
+    }
+  }
+}
---
+++ ViewsExample/Models/PersonAndProductWrapperModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ViewImportsViewsCoreMVC6.Models
+{
+  public class PersonAndProductWrapperModel
+  {
+    public Person PersonData { get; set; }
+    public Product ProductData { get; set; }
+  }
+}
